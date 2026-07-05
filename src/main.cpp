c5ca68a3d9240a83c5fe5f20177115d771985b25
tdkhan <taimurz.khan.16@gmail.com>
2026-07-05 02:45:03 -0500

    TODO 1: User Input

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,21 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
     return std::move(contents);
 }
 
+// function to read user input (does error handling if a wrong input is provided)
+static void readCoordinate(const std::string prompt, float& value) {
+    while(true) {
+        std::cout << prompt;
+
+        if (std::cin >> value) {
+            break;
+        }
+
+        std::cout << "Invalid input, try again." << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+}
+
 int main(int argc, const char **argv)
 {    
     std::string osm_data_file = "";
@@ -52,9 +67,6 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
-
-
  
     // Build Model.
     RouteModel model{osm_data};
@@ -65,7 +77,21 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+
+    float start_x, start_y, end_x, end_y;
+    std::cout << "Enter the coordinates of the start and end points as prompted:" << std::endl;
+    
+    readCoordinate("Enter start_x ", start_x);
+    readCoordinate("Enter start_y ", start_y);
+    readCoordinate("Enter end_x ", end_x);
+    readCoordinate("Enter end_y ", end_y);
+
+    // print the input data for user confirmation
+    std::cout << "\nSetting up the router planner with the following data: \n";
+    std::cout << "start_x, start_y: " << start_x << ", " << start_y << std::endl;
+    std::cout << "end_x, end_y: " << end_x << ", " << end_y << std::endl;
+
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
     
